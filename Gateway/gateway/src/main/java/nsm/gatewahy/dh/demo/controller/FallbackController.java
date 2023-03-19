08c4c6e7c48e98d31a79bb8e7dd377a93c5bbcf0
nsmtechdev <nsmtechdev@gmail.com>
2023-03-19 15:55:18 -0300

    movies2

---
+++ Gateway/gateway/src/main/java/nsm/gatewahy/dh/demo/controller/FallbackController.java
@@ -0,0 +1,35 @@
+package nsm.gatewahy.dh.demo.controller;
+
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @CircuitBreaker(name = "movieService")
+    @GetMapping("/movies")
+    public ResponseEntity<String> moviesFallback() {
+        return new ResponseEntity<>("Movie Service is currently unavailable. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @CircuitBreaker(name = "usersService")
+    @GetMapping("/users")
+    public ResponseEntity<String> catalogsFallback() {
+        return new ResponseEntity<>("Users Service is currently unavailable. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+
+
+
+
+
+
+
+
